0d9e71f3d868fb7eccaad08a0f61b3a3dae99e34
Neil Murphy <wnmurphy@gmail.com>
2025-04-16 18:18:30 -0700

    Add string>number parser, add threshold vars, and make spoken alerts optional.

--- options_screener.py
+++ options_screener.py
@@ -14,85 +14,12 @@ from api_keys import (
     PUSHOVER_USER_KEY,
 )
 
-TESTING = False
+from example_responses import example_response_1 as mock_response
 
-mock_response = {
-    "responseTime": "April 13, 2025 17:04:34 PM EDT",
-    "errorMessage": {
-        "errorCode": "",
-        "errorMessage": "",
-        "detailedErrorMessage": ""
-    },
-    "ScreenData": {
-        "screenid": 1,
-        "underliercount": 149,
-        "optionscount": 1000,
-        "symbollist": [
-            "CMPX",
-            "CHPT"
-        ],
-        "underlierSortColumn": "iv30",
-        "underlierSortDir": "DESC",
-        "optionSortColumn": "tvalx",
-        "optionSortDirection": "DESC",
-        "underlierLimitReached": "N",
-        "securityType": "EQ",
-        "underliers": [
-            {
-                "symbol": "CMPX",
-                "price": "1.695",
-                "vol": "574,697",
-                "avovol": "10,246.433",
-                "iv30": "216.194",
-                "underlying.trade.price": "1.695",
-                "underlying.trade.time": "1744401600180",
-                "options": [
-                    {
-                        "symbol": "CMPX--250417C00004000",
-                        "displaySymbol": "CMPX Apr 17 '25 $4 Call",
-                        "trade.price": "0.05",
-                        "trade.time": "1744378206672",
-                        "ovol": "120",
-                        "ooi": "18,046",
-                        "otype": "CALL",
-                        "ask": "0.05",
-                        "bid": "0",
-                        "strp": "4",
-                        "strm": "2.381",
-                        "tvalx": "100",
-                        "exp": "3"
-                    }
-                ]
-            },
-            {
-                "symbol": "CHPT",
-                "price": "0.60",
-                "vol": "8,264,233",
-                "avovol": "4,166.342",
-                "iv30": "199.651",
-                "underlying.trade.price": "0.6057",
-                "underlying.trade.time": "1744412400001",
-                "options": [
-                    {
-                        "symbol": "CHPT--250509C00001000",
-                        "displaySymbol": "CHPT May 09 '25 $1 Call",
-                        "trade.price": "0.01",
-                        "trade.time": "1744380198521",
-                        "ovol": "249",
-                        "ooi": "611",
-                        "otype": "CALL",
-                        "ask": "0.05",
-                        "bid": "0",
-                        "strp": "1",
-                        "strm": "1.664",
-                        "tvalx": "100",
-                        "exp": "25"
-                    }
-                ]
-            }
-        ]
-    }
-}
+TESTING = False
+SPEAK = True
+TOTAL_TRADE_SIZE_THRESHOLD = 1000 # $1000
+DAYS_TO_EXP_THRESHOLD = 60 # days
 
 
 def parse_curl_string_to_dict(curl_string):
@@ -131,6 +58,7 @@ def parse_curl_string_to_dict(curl_string):
         "query_params": query_params
     }
 
+
 def clean_int(val):
     val = str(val).strip()
     if not val or val in ['--', 'NaN']:
@@ -139,6 +67,7 @@ def clean_int(val):
         return int(re.sub(r'[^\d]', '', val))  # removes commas and non-digits
     except ValueError:
         return 0
+
 
 def clean_float(val):
     val = str(val).strip()
@@ -149,20 +78,34 @@ def clean_float(val):
     except ValueError:
         return 0.0
 
+
+def clean_option_object(opt):
+    # Parse values from the option object from strings to floats/ints.
+    opt['trade.price'] = clean_float(opt['trade.price'])
+    opt['trade.time'] = clean_int(opt['trade.time'])
+    opt['ask'] = clean_float(opt['ask'])
+    opt['bid'] = clean_float(opt['bid'])
+    opt['strp'] = clean_float(opt['strp'])
+    opt['ovol'] = clean_int(opt['ovol'])
+    opt['ooi'] = clean_int(opt['ooi']) if 'ooi' in opt else 0
+    opt['exp'] = clean_int(opt['exp'])
+    return opt
+
+
+def say(msg):
+    if SPEAK:
+        os.system(f'say "{msg}"')
+
+
 # Returns a boolean of whether this qualifying option meets additional criteria.
 def is_high_quality_hit(opt, underlying_price):
     # Parse values
-    trade_price = clean_float(opt['trade.price'])
-    ask_price = clean_float(opt['ask'])
-    strike = clean_float(opt['strp'])
-    volume = clean_int(opt['ovol'])
-    open_interest = clean_int(opt['ooi']) if 'ooi' in opt else 0
-    dte = clean_int(opt['exp'])
-
-    # Total price paid for the position.
-    opt['total_premium'] = trade_price * clean_int(opt['ovol']) * 100
-    # Format as currency
-    opt['total_premium'] = f"${opt['total_premium']:.2f}"
+    trade_price = opt['trade.price']
+    ask_price = opt['ask']
+    strike = opt['strp']
+    volume = opt['ovol']
+    open_interest = opt['ooi']
+    days_to_expiration = opt['exp']
 
     # Derived metrics
     oi_ratio = volume / open_interest if open_interest > 0 else 0
@@ -180,14 +123,13 @@ def is_high_quality_hit(opt, underlying_price):
         oi_ratio >= 1.5 if oi_ratio else True and
         trade_price < 1.00 and
         otm_percent <= 0.05 and
-        dte <= 8 and
         ask_fill
     )
 
 
 # Parses the dict for a hit into a short string message for notification.
 def format_msg_from_hit(hit):
-    return f"{hit['opt']}\ncurrent_share_price: {hit['sh_pr']}\notm_percentage: {hit['otm_perc']}\ndays_to_exp: {hit['exp']}\ntrade_price: {hit['trade_price']}\ntotal_cost: {hit['t_prm']}\ntotal_size: {hit['ovol']}\nhq_hit: {hit['hq_hit']}"
+    return f"{hit['opt']}\ncurrent_share_price: {hit['sh_pr']}\notm_percentage: {hit['otm_perc']}\ndays_to_exp: {hit['exp']}\ntrade_price: {hit['trade_price']}\ntotal_cost: ${hit['t_prm']:,.0f}\ntotal_size: {hit['ovol']:,}\nhq_hit: {hit['hq_hit']}"
 
 
 # Sends a push notification for a given message.
@@ -236,12 +178,12 @@ def main():
 
             if response.status_code == 401:
                 print(f"Error: {response.status_code}")
-                os.system('say "Re-authentication required."')
+                say("Re-authentication required.")
                 break
 
             if response.status_code != 200:
                 print(f"Error: {response.status_code}")
-                os.system(f'say "Error occurred. Got status code {response.status_code}"')
+                say(f"Error occurred. Got status code {response.status_code}")
                 break
 
             cookies.update(response.cookies.get_dict())
@@ -255,10 +197,10 @@ def main():
         print(f"Checking for hits at: {data['responseTime']}")
 
         if "ScreenData" not in data:
-            os.system('say "No hits found."')
+            say("No hits found.")
 
         if "ScreenData" in data:
-            os.system('say "Unusual options trading activity found."')
+            say("Unusual options trading activity found.")
             print(data)
 
             list_of_hits = data.get("ScreenData", {}).get("underliers", [])
@@ -268,13 +210,27 @@ def main():
                 underlying_price = clean_float(hit.get("price"))
                 for option in hit.get("options", []):
 
-                    # Filter out options in the chain with no volume; irrelevant.
-                    if not clean_int(option["ovol"]):
+                    # Convert string values to numbers.
+                    option = clean_option_object(option)
+
+                    # Filter out options in the returned chain with no activity.
+                    if not option["ovol"]:
                         continue
 
-                    # Filter out anything that isn't "buying to open"; irrelevant.
-                    trade_price_higher_than_ask = clean_float(option["trade.price"]) >= clean_float(option["ask"])
-                    trade_volume_higher_than_oi = clean_int(option["ovol"]) > clean_int(option["ooi"])
+                    # Calculate total price paid for the position.
+                    option['total_premium'] = option['trade.price'] * option['ovol'] * 100
+
+                    # Filter out smaller positions.
+                    if option['total_premium'] < TOTAL_TRADE_SIZE_THRESHOLD:
+                        continue
+
+                    # Filter out any options too far out.
+                    if option["exp"] > DAYS_TO_EXP_THRESHOLD:
+                        continue
+
+                    # Filter out any trade that isn't "buying to open" a position.
+                    trade_price_higher_than_ask = option["trade.price"] >= option["ask"]
+                    trade_volume_higher_than_oi = option["ovol"] > option["ooi"]
                     if not trade_price_higher_than_ask and not trade_volume_higher_than_oi:
                         continue
 
@@ -283,9 +239,9 @@ def main():
                         "ovol": option["ovol"],
                         "sh_pr": hit.get("price"),
                         "exp": option.get("exp"),
-                        "hq_hit": is_high_quality_hit(option, underlying_price),
                         "t_prm": option.get("total_premium", 0),
                         "trade_price": option.get("trade.price", 0),
+                        "hq_hit": is_high_quality_hit(option, underlying_price),
                         "otm_perc": option.get("otm_percent", 0),
                     })
 
